91c300e69965ff881afb349020e0158c91adb3dd
Jeffrey S. Evans <jeffreyevans@users.noreply.github.com>
2018-02-01 15:27:37 -0700

    Add files via upload

---
+++ R/classBreaks.R
@@ -0,0 +1,60 @@
+#' @title Class breaks
+#' @description Finds class breaks in a distribution
+#'
+#' @param x
+#' @param n
+#' @param type  Statistic used to find breaks c("equal", "quantile", "std", "geometric")
+#'
+#' @return A vector contaning class break values the length is n+1 to allow for specification of ranges
+#'
+#' @note The robust std method uses sqrt(sum(x^2)/(n-1)) to center the data before deriving "pretty" breaks.
+#'
+#' @author Jeffrey S. Evans  <jeffrey_evans@@tnc.org>
+#' 
+#' @examples
+#'  y <- rnbinom(100, 10, 0.5)
+#'    classBreaks(y, 10)  
+#'    classBreaks(y, 10, type="quantile")
+#'  
+#'    par(mfrow=c(2,2))
+#'      d <- density(y)
+#'      plot(d, type="n", main="Equal Area breaks")
+#'        polygon(d, col="cyan")
+#'        abline(v=classBreaks(y, 10)) 
+#'      plot(d, type="n", main="Quantile breaks")
+#'        polygon(d, col="cyan")
+#'        abline(v=classBreaks(y, 10, type="quantile"))
+#'      plot(d, type="n", main="Robust Standard Deviation breaks")
+#'        polygon(d, col="cyan")
+#'        abline(v=classBreaks(y, 10, type="std"))
+#'      plot(d, type="n", main="Geometric interval breaks")
+#'        polygon(d, col="cyan")
+#'        abline(v=classBreaks(y, 10, type="geometric"))
+#'  	
+#'  ( y.breaks <- classBreaks(y, 10) )   	
+#'  cut(y, y.breaks, include.lowest = TRUE, labels = 1:10)
+#'
+#' @export
+classBreaks <- function(x, n, type = c("equal", "quantile", "std", "geometric") ) {
+  if(type[1] == "equal") { 
+    return( seq(min(x), max(x), length.out=(n+1)) )
+  } else if(type[1] == "quantile") {	
+    return( c(quantile(x=x, probs=seq(0,1,1/10))) )
+  } else if(type[1] == "std") {
+    svar <- scale(x)
+    return( c((pretty(x = svar, n = n) * attr(svar, "scaled:scale")) + 
+               attr(svar, "scaled:center")) )
+  } else if(type[1] == "geometric") {
+    breaks <- c(min(x), max(x))
+      r <- exp((log(max(x)) - log(min(x)))/n)
+        i.min <- breaks[1]
+          for (i in 1:(n - 1)) {
+            breaks <- c(breaks, i.min * r)
+              i.min <- i.min * r
+            breaks <- sort(breaks)
+            }
+    return(breaks)
+  } else {
+    stop("Not a valid statistic")
+  }
+}
---
+++ R/remove.holes.R
@@ -0,0 +1,40 @@
+#' @title Remove polygon holes
+#' @description Removes all holes (null geometry) in polygon sp class objects 
+#'
+#' @param SpatialPolygons or SpatialPolygonsDataFrame class object
+#'
+#' @return SpatialPolygonsDataFrame object with all holes removed
+#'
+#' @note A hole is considered a polygon within a polygon representing null geometry 
+#'
+#' @author Jeffrey S. Evans  <jeffrey_evans@@tnc.org>
+#' 
+#' @examples
+#'  library(sp)
+#'  Sr1 = Polygon(cbind(c(2,4,4,1,2),c(2,3,5,4,2)))
+#'  Sr2 = Polygon(cbind(c(5,4,2,5),c(2,3,2,2)))
+#'  Sr3 = Polygon(cbind(c(4,4,5,10,4),c(5,3,2,5,5)))
+#'  Sr4 = Polygon(cbind(c(5,6,6,5,5),c(4,4,3,3,4)), hole = TRUE)
+#'  polys <- SpatialPolygons(list(Polygons(list(Sr1), "s1"),
+#'                  Polygons(list(Sr2), "s2"),
+#'                  Polygons(list(Sr3, Sr4), "s3/4")), 1:3)
+#'    par(mfrow=c(1,2))
+#'      plot(polys, col = 1:3, main="with hole")
+#'      plot(remove.holes(polys), col = 1:3, main="with hole removed")
+#'
+#' @export
+remove.holes <- function(x) {
+  xp <- slot(x, "polygons")
+    holes <- lapply(xp, function(x) sapply(methods::slot(x, "Polygons"), methods::slot, "hole"))
+    res <- lapply(1:length(xp), function(i) methods::slot(xp[[i]], "Polygons")[!holes[[i]]])
+    IDs <- row.names(x)
+  x.fill <- sp::SpatialPolygons(lapply(1:length(res), function(i)
+                                sp::Polygons(res[[i]], ID=IDs[i])), 
+  						        proj4string=CRS(proj4string(x)))
+  methods::slot(x.fill, "polygons") <- lapply(methods::slot(x.fill, "polygons"), 
+                maptools::checkPolygonsHoles)   
+  methods::slot(x.fill, "polygons") <- lapply(methods::slot(x.fill, "polygons"), "comment<-", NULL)   
+    pids <- sapply(methods::slot(x.fill, "polygons"), function(x) methods::slot(x, "ID"))
+    x.fill <- sp::SpatialPolygonsDataFrame(x.fill, data.frame(row.names=pids, ID=1:length(pids)))	   
+  return( x.fill )	   
+}
