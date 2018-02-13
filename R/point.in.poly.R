490e32720d5b22330215a494574b4cddb227d444
Jeffrey S. Evans <jeffreyevans@users.noreply.github.com>
2018-02-12 19:09:22 -0700

    Update point.in.poly.R
    Added duplicate argument to control overlay behavior in cases of polygon geometry representing multiple features. If duplicate argument is TRUE and more than one polygon intersection occurs, points will be duplicated (new row added) and all attributes joined. However, if duplicate is FALSE, with duplicate intersections, a new column for each unique intersecting polygon will be returned and the points will not be duplicated. For example, if a point intersect three polygons, three new columns will be added representing the polygons ID.

--- R/point.in.poly.R
+++ R/point.in.poly.R
@@ -4,18 +4,21 @@
 #' @param x         sp SpatialPointsDataFrame or SpatialPoints or sf point object
 #' @param y         sp SpatialPolygonsDataFrame or sf polygon object
 #' @param sp        (TRUE/FALSE) Return an sp class object, else returns sf class object
+#' @param duplicate (TRUE/FALSE) Return duplicated features with more than one polygon intersection
 #' @param ...       Additional arguments passed to sf::st_join  
 #'
 #' @return A SpatialPointsDataFrame or sf 
 #'
-#' @note Depends: sp
-#' @note If poly.id is NULL a column "pids" is created based on the rownames of the polygons
+#' @note 
+#' If duplicate argument is TRUE and more than one polygon intersection occurs, points will be duplicated (new row added) and all attributes joined.  
+#' However, if duplicate is FALSE, with duplicate intersections, a new column for each unique intersecting polygon will 
+#' be returned and the points will not be duplicated. For example, if a point intersect three polygons, three new columns will be added
+#' representing the polygons ID.    
 #'
 #' @author Jeffrey S. Evans  <jeffrey_evans@@tnc.org>
 #'
 #' @examples
-#'  # sp example - simple one-to-one feature overlay. Returns number of points from 
-#'  #   original data
+#'  #### Simple one-to-one feature overlay. 
 #'  require(sp)
 #'    data(meuse)
 #'    coordinates(meuse) = ~x+y
@@ -48,43 +51,75 @@
 #'  # Count points in each polygon
 #'  tapply(pts.poly$cadmium, pts.poly$pid, FUN=length)    
 #'    
-#'  # sf example - complex many-to-one feature overlay. Recycles points from multiple polygon 
-#'  #   intersections and can return more points than original data 
+#'  #### Complex many-to-one feature overlay. 
 #'  require(sf)
 #'  p <- sf::st_polygon(list(rbind(c(0,0), c(1,0), c(1,1), c(0,1), c(0,0))))
-#'  polys <- st_sf(sf::st_sfc(p, p + c(.8, .2), p + c(.2, .8)))
+#'  polys <- sf::st_sf(sf::st_sfc(p, p + c(.8, .2), p + c(.2, .8)))
 #'  pts <- sf::st_sf(sf::st_sample(polys, size=100))
 #'  
-#'  plot(polys)
-#'    plot(pts)
+#'  # Duplicates points for each new polygon, no attributes so returns IDs for features    
+#'  pts.poly.dup <-  point.in.poly(pts, polys)
+#'    head(pts.poly.dup@data)
+#'    
+#'  # Should throw error due to lack of attributes  
+#'  pts.poly <- point.in.poly(pts, polys, duplicate = FALSE) 
 #'  
-#'  polys$PIDS <- 1:nrow(polys)
-#'  polys$y <- runif(nrow(polys))
-#'  pts$ATT <- 1:nrow(pts)
+#'  # Coerce to sp class objects
+#'  x <- as(pts, "Spatial")
+#'    x <- SpatialPointsDataFrame(x, data.frame(IDS=1:nrow(x), pty=runif(nrow(x))))
+#'  y <- as(polys, "Spatial")
+#'    y <- SpatialPolygonsDataFrame(y, data.frame(IDS=1:nrow(y), py=runif(nrow(y))))
 #'  
-#'  # Point in polygon overlay
-#'  pts.poly <-  point.in.poly(pts, polys)
-#'    head(pts.poly@data)
+#'  # Returns point attributes with column for each unique polygon   
+#'  pts.poly <- point.in.poly(x, y, duplicate = FALSE)
+#'    head(pts.poly@data)  
+#'    
+#'  # Duplicates points for each new polygon, joins all attributes  
+#'  pts.poly.dup <-  point.in.poly(x, y)
+#'    head(pts.poly.dup@data)  
 #'  
 #'  # Count points in each polygon
-#'  tapply(pts.polys$ATT, pts.polys$PIDS, FUN=length)      
+#'  tapply(pts.poly.dup$IDS.x, pts.poly.dup$IDS.y, FUN=length)      
 #'
 #' @import sf 
 #' @export
-point.in.poly <- function(x, y, sp = TRUE, ...) {
+point.in.poly <- function(x, y, sp = TRUE, duplicate = TRUE, ...) {
   if(!any(class(x)[1] == c("SpatialPoints", "SpatialPointsDataFrame", "sf"))) {
-      stop("x is not a suitable point feature object class") }
+    stop("x is not a suitable point feature object class") }
   if(!any(class(y)[1] == c("SpatialPolygons", "SpatialPolygonsDataFrame", "sf"))) {
-      stop("y is not a suitable polygon feature object class") }
-  if(any(class(x) == "sfc")) { x <- sf::st_sf(x) }
+    stop("y is not a suitable polygon feature object class") }
+  if(any(class(x) == "sfc")) { x <- sf::st_sf(x) }  
+	if(duplicate == FALSE) {
+	  if(!any(class(x)[1] == c("SpatialPoints", "SpatialPointsDataFrame"))) {
+	    x <- as(x, "Spatial")
+		  if(dim(x@data)[2] == 0) stop("There are no attributes associated with points")
+	  }	
+      if(!any(class(y)[1] == c("SpatialPolygons", "SpatialPolygonsDataFrame"))) {
+	    y <- as(y, "Spatial")
+		  if(dim(y@data)[2] == 0) stop("There are no attributes associated with polygons")
+	  }
+      o <- over(x, y, returnList = TRUE)
+      m <- max(unlist(lapply(o, nrow)))
+        ids <- row.names(y)
+          xy <- data.frame(t(sapply(1:length(o), 
+            function(i) c(ids[i], c(o[[i]][,1], rep(NA, m))[1:m])
+          )))
+        colnames(xy) <- c("p",paste0("pid", 1:m))
+      x@data <- data.frame(x@data, xy)
+	    if( sp == FALSE ) sf::st_as_sf(x) 
+	  return( x )
+    } else {
   if(any( class(x) == c("SpatialPoints", "SpatialPointsDataFrame"))) {
-    x <- st_as_sf(x) 
+    x <- sf::st_as_sf(x)
   }
   if(any(class(y) == "sfc")) { x <- sf::st_sf(y) }
   if(any( class(y) == c("SpatialPolygons", "SpatialPolygonsDataFrame"))) {
-    y <- st_as_sf(y)  
-  }  
-    o <- sf::st_join(x, y, ...)
+    y <- sf::st_as_sf(y)	
+  } 
+  if(dim(x)[2] == 1) x$pt.ids <- 1:nrow(x)	
+    if(dim(y)[2] == 1) y$poly.ids <- 1:nrow(y)	  
+      o <- sf::st_join(x, y, ...)
   if( sp ) o <- as(o, "Spatial")
- return( o )  
+  return( o ) 
+  } 
 }
