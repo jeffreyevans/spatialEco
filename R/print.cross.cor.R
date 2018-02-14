dc78566c3d0a7110e0f5ab9de4baf1c6ee4ad099
Jeffrey S. Evans <jeffreyevans@users.noreply.github.com>
2018-02-14 13:09:35 -0700

    Add files via upload

---
+++ R/print.cross.cor.R
@@ -0,0 +1,27 @@
+#' @title Print spatial cross correlation
+#' @description print method for class "cross.cor"
+#' @param x    Object of class cross.cor
+#' @param ...  Ignored
+#'
+#' @method print cross.cor
+#'
+#' @export
+print.cross.cor <- function(x, ...) {
+  if(any(names(x)=="summary")) {
+  cat("Moran's-I under randomization assumptions...", "\n")
+    cat("    First-order Moran's-I: ", x$summary$I, "\n")
+	cat("    Mean(I): ", x$summary$meanI, "\n")
+	cat("    STDV(I): ", x$summary$stdI, "\n")
+  	cat("    p-value (2-sided): ", x$summary$p, "\n")
+	cat("\n", "Summary statistics of local partial cross-correlation", "\n")
+      print(summary(x$lisa))
+	cat("\n", "Counts of cluster types")
+	  print(table(x$cluster))
+  } else {
+    cat("First-order Moran's-I: ", x$I, "\n")
+    cat("\n", "Summary statistics of local partial cross-correlation", "\n")
+      print(summary(x$lisa))
+  	cat("\n", "Counts of cluster types")
+  	  print(table(x$cluster))
+  }
+}
