aa52eb8f4405673a2b3bf454985ab53ca5a15916
Jeffrey S. Evans <jeffreyevans@users.noreply.github.com>
2018-10-22 13:27:42 -0600

    new function
    Distance constrained subsampling

---
+++ R/sample.distance.R
@@ -0,0 +1,82 @@
+#' @title Sample distance
+#' @description Minimum, and optional maximum, distance constrained sub-sampling
+#'
+#' @param x             A spatial polygons or points sp object
+#' @param n             Number of random samples drawn from x
+#' @param d             Minimum sample distance
+#' @param d.max         Maximum sample distance (not used unless specified 
+#' @param replacement  (FALSE/TRUE) sample with or without replacement
+#' @param trace        (FALSE/TRUE) Print min and max sample distances
+#' 
+#' @return A sp spatial object, of the same class as x containing the random sub-samples 
+#'
+#' @note This function provides a distance constrained subsample of existing point or polygon data  
+#'  
+#' @author Jeffrey S. Evans  <jeffrey_evans@@tnc.org>
+#'
+#' @examples
+#'   library(spdep)
+#'   library(sp)
+#'   
+#'   data(meuse)
+#'     coordinates(meuse) <- ~ x+y
+#'   
+#'   # Subsample with a 500m minimum sample spread 
+#'   sub.meuse <- sample.distance(meuse, n=10, d=500, trace = TRUE)  
+#'     plot(meuse,pch=19, main="min dist = 500")
+#'       points(sub.meuse, pch=19, col="red") 
+#'
+#'   # Check distances	
+#'   dm <- spDists(sub.meuse)
+#'     diag(dm) <- NA
+#'   cat("\n", "Min distance for subsample", min(dm, na.rm=T), "\n")  
+#'   cat("Max distance for subsample", max(dm, na.rm=T), "\n")  
+#'     
+#' \dontrun{
+#'   # Subsample with a 500m minimum and 3500m maximum sample spread   
+#'   sub.meuse <- sample.distance(meuse, n=10, d=500, d.max=3500)  
+#'     plot(meuse,pch=19, main="min dist = 500, max dist = 3500")
+#'       points(sub.meuse, pch=19, col="red") 
+#'
+#'   # Check distances		
+#'   dm <- spDists(sub.meuse)
+#'     diag(dm) <- NA
+#'   cat("Min distance for subsample", min(dm, na.rm=T), "\n")  
+#'   cat("Max distance for subsample", max(dm, na.rm=T), "\n")    
+#' }
+#' 
+#' @export	
+sample.distance <- function(x, n, d, d.max = NULL, replacement = FALSE,
+                            latlong = FALSE, trace = FALSE) {
+  if(missing(x)) stop("Must define a spatial object")
+    if(missing(d)) stop("Must define minimum separation distance")
+  if(!any(class(x) == c("SpatialPointsDataFrame", "SpatialPolygonsDataFrame")) )
+    stop("x must be sp class polygons or points")
+  if(latlong == TRUE)  
+    message("Note; geographic projection (lat-long) distances must be in kilometers")  
+  rs <- sample(1:nrow(x),1) 
+    s <- x[rs,]
+      if(replacement == FALSE) { x <- x[-rs,] }	
+	  distance = TRUE 
+        for(i in 2:n) {
+          while(distance == TRUE) {	
+		    rs <- sample(1:nrow(x),1)
+            if(is.null(d.max)) {
+              distance <- any(sp::spDistsN1(s, x[rs,], longlat = latlong) < d)
+		    } else {
+		      distance <- any(sp::spDistsN1(s, x[rs,], longlat = latlong) < d) |
+		                  any(sp::spDistsN1(s, x[rs,], longlat = latlong) > d.max)
+		    }		  
+	      }
+		  if(trace) {
+		    cat("\n","Min distance for", i, "=", min(sp::spDistsN1(s, x[rs,], 
+		       longlat = latlong)), "\n")
+            cat(" Max distance for", i, "=", max(sp::spDistsN1(s, x[rs,], 
+		        longlat = latlong)), "\n")
+		  }		
+	        distance = TRUE
+	      s <- rbind(s, x[rs,])
+	    if(replacement == FALSE) { x <- x[-rs,] } 
+	    }  
+  return(s)  
+}
