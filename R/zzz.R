e20be8a3660c89d702f16774285afaff52bc6437
Jeffrey S. Evans <jeffreyevans@users.noreply.github.com>
2018-12-17 12:29:12 -0700

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 #' @title Package startup message
 #' @description Displays package version and news file 
 #' @export
-.onAttach <- function(libname, pkgname) {
-  se.ver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
-                     fields="Version")
-  packageStartupMessage(paste(pkgname, se.ver))
-  packageStartupMessage("Type se.news() to see new features/changes/bug fixes.")
-}
+#.onAttach <- function(libname, pkgname) {
+#  se.ver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
+#                     fields="Version")
+#  packageStartupMessage(paste(pkgname, se.ver))
+#  packageStartupMessage("Type se.news() to see new features/changes/bug fixes.")
+#}
