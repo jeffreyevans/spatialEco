9c637a61fb345fa573274db390b35b1c1a4e109e
Jeffrey Evans <jeffrey_evans@tnc.org>
2019-10-30 17:17:08 -0600

    updates

--- R/spatial.select.R
+++ R/spatial.select.R
@@ -6,8 +6,10 @@
 #' @param y           A sp or sf feature class that will be subset by the query of x
 #' @param distance    A proximity distance of features to select (within distance)
 #' @param predicate   Spatial predicate for intersection
+#' @param neighbors   If predicate = "contingency" type of neighbors options are c("queen", "rook")
 #'
-#' @return An sp object representing a subset of y based on the spatial query of x
+#' @return An sp object representing a subset of y based on the spatial query of x or, 
+#'         if predicate = contingency a sparse matrix representing neighbor indexes
 #'
 #' @note Performs a spatial select of features based on an overlay of a polygon (x),   
 #'       which can represent multiple features, and a polygon, point or line feature 
@@ -16,8 +18,8 @@
 #'       query polygon. This function is similar to ArcGIS/Pro spatial select. Please note
 #'       that for point to point neighbor selections use the knn function. 
 #'
-#' @note Valid spatial predicates include: intersect, touches, covers, contains and 
-#'       proximity. Please see \url{https://en.wikipedia.org/wiki/DE-9IM/} for detailed 
+#' @note Valid spatial predicates include: intersect, touches, covers, contains, proximity and 
+#'       contingency. Please see \url{https://en.wikipedia.org/wiki/DE-9IM/} for detailed 
 #'       information on the DE-9IM topology model. 
 #'
 #' @author Jeffrey S. Evans  <jeffrey_evans@@tnc.org> 
@@ -70,7 +72,14 @@
 #'   plot(sub.contains, main="contains", pch=20)
 #'     plot(p, add=TRUE) 
 #'   plot(sub.prox, main="Proximity 200m distance", pch=20)
-#'     plot(p, add=TRUE) 
+#'     plot(p, add=TRUE)
+#'
+#' #### For rook or queen polygon contingency 	
+#' spolys <- as(sf::st_make_grid(sf::st_sfc(sf::st_point(c(0,0)), 
+#'              sf::st_point(c(3,3))), n = c(3,3)), "Spatial")
+#' 
+#' spatial.select(spolys, predicate = "contingency")
+#' spatial.select(spolys, predicate = "contingency", neighbors = "rook") 
 #'
 #' @seealso \code{\link[rgeos]{gIntersects}} for details on intersect predicate
 #' @seealso \code{\link[rgeos]{gContains}} for details on contain predicate
@@ -80,9 +89,10 @@
 #' @seealso \url{https://en.wikipedia.org/wiki/DE-9IM/} for details on DE-9IM topology model
 #'
 #' @export spatial.select	
-spatial.select <- function(x, y, distance = NULL, predicate = c("intersect",   
-                           "contains", "covers", "touches", "proximity")) {
-
+spatial.select <- function(x, y = NULL, distance = NULL, predicate = c("intersect",   
+                           "contains", "covers", "touches", "proximity",
+						   "contingency"), neighbors = c("queen", "rook")) {
+  if(!is.null(y)) {
   if(any(methods::is(x, "Spatial"), methods::is(y, "Spatial"))) {
     type="dsp"
     if(!any(class(x) == c("SpatialPolygons", "SpatialPolygonsDataFrame"))) 
@@ -97,7 +107,17 @@ spatial.select <- function(x, y, distance = NULL, predicate = c("intersect",
       stop("y must be a sf polygon, point or line object")   
   } else {
     stop("x and y need to be sp or sf spatial objects")
-  }			
+  }	
+  } else {
+    if(!any(methods::is(x, "Spatial")))
+	  stop("x must be a spatial object")
+	if(!any(class(x) == c("SpatialPolygons", "SpatialPolygonsDataFrame"))) 
+      stop("x must be a sp polygon object")  
+	if(predicate != "contingency")
+      stop("The only predicate that supports self realization is contingency")	
+    if(!any(class(x) == c("SpatialPolygons", "SpatialPolygonsDataFrame"))) 
+      stop("x must be a sp polygon object")	  
+  }
   if(predicate == "intersect") {
     if(type == "dsp") {
  	  idx <- rgeos::gIntersects(x, y, byid=TRUE)
@@ -126,12 +146,17 @@ spatial.select <- function(x, y, distance = NULL, predicate = c("intersect",
     } else if(type == "dsf") {
       idx <- sort(unique(unlist(sf::st_touches(x,y))))	
 	} 
+  } else if(predicate == "contingency") {
+    if(neighbors[1] == "rook"){ adj = "F***1****"} else {adj = "F***T****"}
+      cont <- function(a, b = a, adj) sf::st_relate(a, b, pattern = adj)
+	    y <- cont(as(x, "sf"), adj = adj) 
+          return(y)
   } else if(predicate == "proximity") {
     if(is.null(distance)) 
 	  stop("For proximity predicate distance must be defined")
     if(type == "dsp") {   	  
       idx <- rgeos::gWithinDistance(x, y, dist=distance, 
-                               byid=TRUE, densifyFrac=1)
+                                    byid=TRUE, densifyFrac=1)
 	    idx <- unique(which(idx == TRUE, arr.ind = TRUE)[,1])
     } else if(type == "dsf") {
       idx <- sort(unique(unlist(sf::st_is_within_distance(x,y, dist = distance))))	 
