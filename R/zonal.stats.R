1652c8cfc2b80a30e6fe5751c101048bffb72b1b
Jeffrey Evans <jeffrey_evans@tnc.org>
2019-11-16 13:46:28 -0700

    Enhancements

--- R/zonal.stats.R
+++ R/zonal.stats.R
@@ -1,32 +1,32 @@
 #' @title zonal.stats
 #' @description Polygon zonal statistics of a raster 
 #'
-#' @param x  Polygon object of class SpatialPolygonsDataFrame 
-#' @param y  Raster object of class raster
-#' @param stat  Statistic or function
-#' @param trace  Should progress counter be displayed
-#' @param plot  Should subset polygons/rasters be plotted (TRUE/FALSE)
+#' @param x      Polygon object of class SpatialPolygonsDataFrame 
+#' @param y      rasterLayer object of class raster
+#' @param stats  Statistic or function
 #'
 #' @return 
-#' Vector, length of nrow(x), of function results
+#' data.frame, nrow(x) and ncol of function results
 #'
 #' @note
-#' This function iterates through a polygon object, masks the raster to each subset polygon and then coerces the subset raster to a vector object. The resulting vector is then passed to the specified statistic/function. This is much slower than zonal functions available in GIS software but has the notable advantage of being able to accept any custom function, passed to the 'stat' argument, appropriate for a vector object (see example).    
+#' This function calculates the zonal statistics between a polygon vector object and a raster. 
+#'   This provides the advantage of being able to accept any custom function, passed to the 'stats' 
+#    argument. Please note that any custom function needs to have a 'na.rm' argument.     
 #'
-#' @note Depends: sp, raster
+#' @note Depends: sp, raster, velox
 #'
 #' @author Jeffrey S. Evans  <jeffrey_evans@@tnc.org>
 #'
 #' @examples                                                                       
 #' # skewness function
-#' skew <- function(x, na.rm=FALSE) { 
+#' skew <- function(x, na.rm = FALSE) { 
 #'    if (na.rm) 
 #'        x <- x[!is.na(x)]
 #'   sum( (x - mean(x)) ^ 3) / ( length(x) * sd(x) ^ 3 )  
 #'   }   
 #' 
 #' # percent x >= p function
-#' pct <- function(x, p=0.30) {
+#' pct <- function(x, p=0.30, na.rm = FALSE) {
 #'   if ( length(x[x >= p]) < 1 )  return(0) 
 #'     if ( length(x[x >= p]) == length(x) ) return(1) 
 #'      else return( length(x[x >= p]) / length(x) ) 
@@ -44,38 +44,34 @@
 #'   plot(p, add=TRUE, lwd=4) 
 #' 
 #' # run zonal statistics using skew and pct functions   
-#' z.skew <- zonal.stats(x=p, y=r, stat=skew, trace=TRUE, plot=TRUE) 
-#' z.pct <- zonal.stats(x=p, y=r, stat=pct, trace=TRUE, plot=TRUE)
-#'   ( z <- data.frame(ID=as.numeric(as.character(row.names(p@@data))), 
+#' z.skew <- zonal.stats(x = p, y = r, stats = "skew") 
+#' z.pct <- zonal.stats(x=p, y=r, stats = "pct")
+#'   ( z <- data.frame(ID = as.numeric(as.character(row.names(p@@data))), 
 #'                     SKEW=z.skew, PCT=z.pct) )  
 #'
+#' @import velox
 #' @export
-zonal.stats <- function(x, y, stat, trace = TRUE, plot = TRUE) {
+zonal.stats <- function(x, y, stats = c("min", "mean", "max")) {
   # if(class(x) == "sf") { x <- as(x, "Spatial") }
     if (class(y) != "RasterLayer") 
         stop("y must be a raster object")
     if (class(x) != "SpatialPolygonsDataFrame") 
         stop("x must be a SpatialPolygonsDataFrame object")
-    results <- vector()
-    for (j in 1:nrow(x)) {
-      if (trace == TRUE) {
-          cat("processing", j, "of", nrow(x), "\n")
-      }
-      lsub <- x[j, ]
-        cr <- raster::crop(y, raster::extent(lsub), snap = "out")
-	    fr <- fasterize::fasterize(sf::st_as_sf(lsub), cr)
-        #fr <- raster::rasterize(lsub, cr)
-      r <- raster::mask(x = cr, mask = fr)
-        if (plot == TRUE) {
-          plot(r, main = paste("Polygon: ", j, sep = " "))
-        }
-        r <- raster::values(r)
-      r <- stats::na.omit(r)
-      if (length(r) < 1) {
-        results <- append(results, NA)
-      } else {
-        results <- append(results, stat(r))
-      }
+  rvx <- velox::velox(y) 
+    ldf <- rvx$extract(sp = x)
+	  names(ldf) <- row.names(x)
+	  
+    stats.fun <- function(x, m = stats) {
+	  slist <- list()
+        for(i in 1:length(m)) {
+	      slist[[i]] <- apply(x, MARGIN=2, m[i], na.rm=TRUE)
+	    }	
+	  return( as.data.frame(t(unlist(slist))) )
     }
-    results
+        results <- lapply(ldf, FUN=stats.fun)
+	  results <- do.call("rbind", results)
+	rn <- vector()
+	  for(n in stats) { rn <- append(rn, paste(n, names(y), sep="."))}
+	    names(results) <- rn 
+  return( results )
 } 
