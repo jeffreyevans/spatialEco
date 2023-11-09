698bd3987208ec828076d6a3f55a866b46fad4f0
Jeffrey S. Evans <jeffreyevans@users.noreply.github.com>
2023-11-09 11:15:59 -0700

    Update chae.R

--- R/chae.R
+++ R/chae.R
@@ -12,18 +12,18 @@
 #' @author Jeffrey S. Evans  <jeffrey_evans@@tnc.org>
 #'
 #' @examples
- dat <- data.frame(DogAge = seq(0,18,0.25),
-              HumanAge=chae(seq(0,18,0.25)))[-1,]
- 
- plot(dat$DogAge, dat$HumanAge, "l",
-      main="Canine-Human Age Equivalence",
- 	 ylab="Human Age", xlab="Dog Age")
-   points( 15, chae(15), col="red", pch=19, cex=1.5)
-   points( 10, chae(10), col="blue", pch=19, cex=1.5)
-   points( 3, chae(3), col="black", pch=19, cex=1.5)
- legend("bottomright", legend=c("Camas (15-YO)", "Kele (10-YO)", "Aster (3-YO)"), 
-       pch=c(19,19,19), cex=c(1.5,1.5,1.5), 
- 	     col=c("red","blue","black"))  
- 
+#' dat <- data.frame(DogAge = seq(0,18,0.25),
+#'              HumanAge=chae(seq(0,18,0.25)))[-1,]
+#' 
+#' plot(dat$DogAge, dat$HumanAge, "l",
+#'      main="Canine-Human Age Equivalence",
+#' 	 ylab="Human Age", xlab="Dog Age")
+#'   points( 15, chae(15), col="red", pch=19, cex=1.5)
+#'   points( 10, chae(10), col="blue", pch=19, cex=1.5)
+#'   points( 3, chae(3), col="black", pch=19, cex=1.5)
+#' legend("bottomright", legend=c("Camas (15-YO)", "Kele (10-YO)", "Aster (3-YO)"), 
+#'       pch=c(19,19,19), cex=c(1.5,1.5,1.5), 
+#' 	     col=c("red","blue","black"))  
+#' 
 #' @export  
 chae <- function(x) { round(16 * log(x) + 31, 2) }
